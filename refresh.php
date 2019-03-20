86d0367524adb912f99ee8776443215b95796098
Zhaiyuqing2003 <48752879+Zhaiyuqing2003@users.noreply.github.com>
2019-03-20 21:46:23 +0800

    studentselector 0.0.2

--- refresh.php
+++ refresh.php
@@ -1,10 +1,9 @@
 <?php
      $con = mysqli_connect("localhost:3306",'root','15926807');
      mysqli_select_db($con,"bigproject");
-     $result = mysqli_fetch_all(mysqli_query($con,"SELECT name FROM choose")) ;
+     $result = mysqli_fetch_all(mysqli_query($con,"SELECT name,img FROM choose")) ;
      $output = [];
      foreach ($result as $value){
-        array_push($output,$value[0]);
+        echo "$value[0],$value[1];";
      };
-     print_r($output);
 ?>
