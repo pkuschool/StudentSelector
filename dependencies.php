757f6ff99aafeebb28836220fa34340133701ea2
YC L <30719670+smallg0at@users.noreply.github.com>
2019-03-24 12:36:08 +0800

    fix spell error

--- dependencies.php
+++ dependencies.php
@@ -1,7 +1,7 @@
 <?php
 //这个页面用来暴力引用dependencies。使用时直接include_once()即可。
 echo '<link rel="stylesheet" href="./css/normalize.css">';
-echo '<link rel="stylesheet" href="./css/matericlize.css">';
+echo '<link rel="stylesheet" href="./css/materialize.css">';
 echo '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
 echo '<script src="./js/anijs.js"></script>';
 echo '<script src="./js/materialize.js"></script>';
