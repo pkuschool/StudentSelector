518c85f2b792bc1552a66fef814eb1aaf324e0be
YC L <30719670+smallg0at@users.noreply.github.com>
2019-04-01 22:20:48 +0800

    edit title

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>主页</title>
+    <title>管理 | 点名系统</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <?php
