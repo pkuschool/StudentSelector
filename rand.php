7e3ea3d6bb4fe59da5acdc535d37fa95bfeeebfc
YC L <30719670+smallg0at@users.noreply.github.com>
2019-04-10 23:08:50 +0800

    add delay to random Now configured to make it more natural.

--- rand.php
+++ rand.php
@@ -6,11 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>随机 | 点名系统</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+        function reloadp() {
+            history.go(0);
+
+        }
+    </script>
 </head>
 
 <body class="blue" style=" font-family: '思源黑体', 'PingFang SC', 'Segoe UI';">
     <div class="container  white-text center centered center-align">
-        <div class=" card-panel white black-text hoverable waves-effect waves-ripple" style="margin-top: 300px;width: 100%" onclick="history.go(0);">
+        <div class=" card-panel white black-text hoverable waves-effect waves-ripple" style="margin-top: 300px;width: 100%" onclick="setTimeout(reloadp,350);">
             <p class=" blue-grey-text" style="font-size: 75px;"><?php
                                                                 session_start();
                                                                 error_reporting(E_ERROR);
@@ -26,7 +32,7 @@
                                                                 echo $o_msg; ?></p>
 
         </div>
-        <div onclick="window.open('main.php','_self')" class="btn blue darken-3 white-text waves-ripple waves-effect"><i class="material-icons">home</i></div>
+        <div onclick="window.open('main.php', '_self')" class="btn blue darken-3 white-text waves-ripple waves-effect"><i class="material-icons">home</i></div>
     </div>
 </body>
 
