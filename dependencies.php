f395327b02d22f1eefeff9ee8e85787025fd1954
YC L <30719670+smallg0at@users.noreply.github.com>
2019-04-28 09:45:03 +0800

    Switch CDN

--- dependencies.php
+++ dependencies.php
@@ -2,7 +2,7 @@
 //这个页面用来暴力引用dependencies。使用时直接include_once()即可。
 echo ' <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 ';
-echo '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">';
+echo '<link href="https://cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.css" rel="stylesheet">';
 echo '<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>';
 echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>';
 echo '
